5f1dfacd15ae3bf9fa9f9401a5e5eeda6019b651
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2013-05-20 12:26:25 -0400

    Changed the structure around to support the maven tools.

---
+++ src/main/java/fantabel/chessmasher/App.java
@@ -0,0 +1,13 @@
+package fantabel.chessmasher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
