005443381f5cbb8a32b12f628f46907ee8471ab9
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2013-05-25 21:39:26 -0400

    Some test instruction in the main class.

--- src/main/java/fantabel/chessmasher/App.java
+++ src/main/java/fantabel/chessmasher/App.java
@@ -1,5 +1,8 @@
 package fantabel.chessmasher;
 
+import fantabel.chessmasher.util.BitBoardUtil;
+import fantabel.chessmasher.board.Board;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	System.out.println(BitBoardUtil.toStringBB(1L));
+	System.out.println("Finally");
+	Board myBoard = new Board();
+	System.out.println(myBoard.toString());
+
     }
 }
